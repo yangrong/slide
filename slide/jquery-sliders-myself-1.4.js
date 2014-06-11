ec5fce3517ddbe75124c3e976d4c664b7ab15baf
yangrong <415390966@qq.com>
2014-06-11 15:45:53 +0800

    新增1.46.7.0.0329

--- slide/jquery-sliders-myself-1.4.js
+++ slide/jquery-sliders-myself-1.4.js
@@ -24,7 +24,7 @@
 	Slide.prototype.option = function(options) {
 		this.option = {
 			//记录第一张聚焦的图片在哪里。
-			fouseNum: 1,
+			fouseNum: 0,
 			speed: 3000,
 			autoplay: true,
 			hasList: true,
@@ -34,44 +34,57 @@
 	//向右运动
 	Slide.prototype.rightMove = function(index) {
 		var _this = this;
-		// console.log('index:', index);
-
+		if (index < 0) index = this.ulContentLi.length - 1;
+		if (index > this.ulContentLi.length - 1) index = 0;
 		if (_this.switch_slide) {
-			_this.switch_slide = false;
-			_this.fouseNum = index;
-			_this.ulContent.animate({
-				'margin-left': '-' + index * _this.cellwidth + 'px'
-			}, function() {
-				if (index > _this.Lilength) {
-					_this.fouseNum = 1;
-					_this.ulContent.css('margin-left', '-' + _this.cellwidth + 'px');
-				}
-				_this.switch_slide = true;
-				_this.ulList.find('.on').removeClass('on');
-				$(_this.navli[_this.fouseNum - 1]).addClass('on');
-			});
 
+			_this.switch_slide = false;
+			var nowUl = this.ulContent.find('li');
+			var fouseNumleft = nowUl.index(this.ulContentLi[_this.fouseNum]);
+			// console.log('fouseNumindex', fouseNumleft);
+			var nextNumleft = nowUl.index(this.ulContentLi[index]);
+			// console.log('nextNumindex', nextNumleft);
+
+			$(this.ulContentLi[index]).insertBefore(this.ulContentLi[_this.fouseNum]);
+			if (fouseNumleft < nextNumleft) {
+				this.ulContent.css('margin-left', '-' + _this.cellwidth + 'px');
+			}
+
+			_this.fouseNum = index;
+			this.ulContent.animate({
+				'margin-left': '+=' + _this.cellwidth + 'px'
+			}, function() {
+				_this.switch_slide = true;
+			});
+			_this.changeClass(_this.fouseNum, 'on');
 		}
+
 	}
 	//向左运动
 	Slide.prototype.leftMove = function(index) {
 		var _this = this;
+		if (index < 0) index = this.Lilength - 1;
 		if (this.switch_slide) {
-			this.switch_slide = false;
+			_this.switch_slide = false;
+			$(this.ulContentLi[index]).insertAfter(this.ulContentLi[_this.fouseNum]);
+			var curMarginLeft = this.ulContent.css('margin-left');
+			if (parseInt(curMarginLeft) == -parseInt(this.cellwidth) * (this.Lilength - 1)) {
+				this.ulContent.css('margin-left', '-' + parseInt(this.cellwidth) * (this.Lilength - 2) + 'px');
+			}
+
 			this.fouseNum = index;
-			_this.ulContent.animate({
-				'margin-left': '-' + _this.fouseNum * _this.cellwidth + 'px'
+			this.ulContent.animate({
+				'margin-left': '-=' + _this.cellwidth + 'px'
 			}, function() {
-				if (_this.fouseNum == 0) {
-					_this.fouseNum = _this.Lilength;
-					_this.ulContent.css('margin-left', '-' + _this.cellwidth * _this.Lilength + 'px')
-				}
 				_this.switch_slide = true;
-				_this.ulList.find('.on').removeClass('on');
-				$(_this.navli[_this.fouseNum - 1]).addClass('on');
 			});
+			_this.changeClass(_this.fouseNum, 'on');
 
 		}
+	}
+	Slide.prototype.changeClass = function(next, classname) {
+		this.ulList.find('.' + classname).removeClass(classname);
+		$(this.navli[next]).addClass(classname);
 	}
 	//自动轮播
 	Slide.prototype.doSlide = function() {
@@ -105,15 +118,30 @@
 		// 鼠标移出开始动画
 
 		_this.wraperBox.on('mouseleave', function() {
-			_this.doSlide(_this.fouseNum + 1);
+			_this.doSlide();
 		});
 
-		//鼠标移入nav
+		//鼠标移入nav的li
 
 		_this.navli.on('mouseenter', function() {
 			clearInterval(_this.t);
-			var index = $(this).attr('data-index');
-			_this.rightMove(parseInt(index));
+			var curli = _this.ulList.find('.on')
+			var preindex = _this.navli.index(curli);
+			// console.log(preindex);
+			var curindex = _this.navli.index($(this));
+			// console.log(curindex);
+			if (preindex < curindex) {
+				_this.rightMove(parseInt(curindex));
+			} else if (preindex > curindex) {
+				_this.leftMove(parseInt(curindex));
+			} else {
+				return;
+			}
+		})
+
+		// 移出nav
+		_this.ulList.on('mouseenter', function() {
+			_this.doSlide();
 		})
 
 
@@ -121,12 +149,8 @@
 	// 执行函数
 	Slide.prototype.init = function() {
 
-		var _this = this,
-			first = _this.ulContentLi.first().clone(),
-			last = _this.ulContentLi.last().clone();
+		var _this = this;
 
-		_this.ulContent.append(first).css('margin-left', '-' + _this.cellwidth + 'px');
-		last.insertBefore(_this.ulContentLi.eq(0));
 		if (_this.autoplay) {
 			_this.doSlide();
 		}
